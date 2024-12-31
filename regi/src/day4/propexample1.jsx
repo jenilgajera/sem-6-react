c98da201c2836c35d5e4e8c742af9d7e5989b3d8
jenilgajera <jenilgajera43@gmail.com>
2024-12-31 12:47:27 +0530

    small changes in day4 file

---
+++ regi/src/day4/propexample1.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+class Propexample1 extends React.Component {
+  render() {
+    const {prop1, xyz} =this.state;
+    
+    return (
+      <div>propexample1</div>
+    )
+  }
+}
+
+
+export default Propexample1;
